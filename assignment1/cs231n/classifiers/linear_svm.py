couldnt find the bug in serialized gradient

assignment1/cs231n/classifiers/linear_svm.py
@@ -35,6 +35,10 @@ def svm_loss_naive(W, X, y, reg):
             margin = scores[j] - correct_class_score + 1  # note delta = 1
             if margin > 0:
                 loss += margin
+                
+                # if margin > 0, this is where gradient happens
+                dW[y[i],:] -= X[i, :] # this is really a sum over j != y_i
+                dW[j,:] += X[i, :] # sums each contribution of the x_i's
 
     # Right now the loss is a sum over all training examples, but we want it
     # to be an average instead so we divide by num_train.
@@ -44,13 +48,14 @@ def svm_loss_naive(W, X, y, reg):
     loss += 0.5 * reg * np.sum(W * W)
 
     #############################################################################
-    # TODO:                                                                     #
     # Compute the gradient of the loss function and store it dW.                #
     # Rather that first computing the loss and then computing the derivative,   #
     # it may be simpler to compute the derivative at the same time that the     #
     # loss is being computed. As a result you may need to modify some of the    #
     # code above to compute the gradient.                                       #
     #############################################################################
+    dW /= num_train
+    dW += reg * W
 
     return loss, dW
 
@@ -64,9 +69,6 @@ def svm_loss_vectorized(W, X, y, reg):
 
     Inputs and outputs are the same as svm_loss_naive.
     """
-    loss = 0.0
-    dW = np.zeros(W.shape)  # initialize the gradient as zero
-
     #############################################################################
     # Implement a vectorized version of the structured SVM loss, storing the    #
     # result in loss.                                                           #
@@ -94,7 +96,6 @@ def svm_loss_vectorized(W, X, y, reg):
     loss += 0.5 * reg * np.sum(W * W)
 
     #############################################################################
-    # TODO:                                                                     #
     # Implement a vectorized version of the gradient for the structured SVM     #
     # loss, storing the result in dW.                                           #
     #                                                                           #
@@ -102,7 +103,16 @@ def svm_loss_vectorized(W, X, y, reg):
     # to reuse some of the intermediate values that you used to compute the     #
     # loss.                                                                     #
     #############################################################################
-    pass
+    binary = thresh > 0  # (m, 10)
+    counting = binary.sum(axis=1)
 
+    # this way, the right class will have it's right negative counting, and the rest are 0/1 
+    binary[np.arange(m), y] = -counting
+
+    # this is the matrix math I dont fully comprehend, feels right
+    dW = np.dot(binary.T, X) / m
+
+    # Gradient regularization that carries through per https://piazza.com/class/i37qi08h43qfv?cid=118
+    dW += reg * W
 
     return loss, dW