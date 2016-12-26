1c0d4564daebc2f6867d67c255deecf0d72a589d
icrtiou <lucas@lucas.ai>
2016-12-26 11:12:25 -0800

    affine layer fwd & bwd

--- assignment2/cs231n/layers.py
+++ assignment2/cs231n/layers.py
@@ -19,16 +19,12 @@ def affine_forward(x, w, b):
   - out: output, of shape (N, M)
   - cache: (x, w, b)
   """
-  out = None
-  #############################################################################
-  # TODO: Implement the affine forward pass. Store the result in out. You     #
-  # will need to reshape the input into rows.                                 #
-  #############################################################################
-  pass
-  #############################################################################
-  #                             END OF YOUR CODE                              #
-  #############################################################################
+  N = x.shape[0]
+  x_reshape = x.reshape(N, -1)
+
+  out = np.dot(x_reshape, w) + b  # broadcasting the +
   cache = (x, w, b)
+
   return out, cache
 
 
@@ -48,14 +44,12 @@ def affine_backward(dout, cache):
   - db: Gradient with respect to b, of shape (M,)
   """
   x, w, b = cache
-  dx, dw, db = None, None, None
-  #############################################################################
-  # TODO: Implement the affine backward pass.                                 #
-  #############################################################################
-  pass
-  #############################################################################
-  #                             END OF YOUR CODE                              #
-  #############################################################################
+  x_reshape = x.reshape(x.shape[0], -1)
+
+  db = dout.sum(axis=0)
+  dw = np.dot(x_reshape.T, dout)  # (D, M) = (N, D).T @ (N, M)
+
+  dx = np.dot(dout, w.T).reshape(x.shape)  # (N, d1, d2....) = (N, M) @ (D, M).T -> reshape
   return dx, dw, db
 
 
